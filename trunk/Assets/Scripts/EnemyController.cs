f98c2de1fe9176554bd970c39e0c1f65b06f16e1
wood.innis <wood.innis@eab2e7ce-034a-a63b-0ccd-4006824de3b7>
2014-09-03 20:49:47 +0000

    Path randomization and patrol functionality implemented. EnemyController.cs allows multiple destinations.

--- trunk/Assets/Scripts/EnemyController.cs
+++ trunk/Assets/Scripts/EnemyController.cs
@@ -3,20 +3,37 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour {
 
-	public Transform target;
+	public Transform[] target;
 
 	private NavMeshAgent agent;
-
+	private int currentDestination;
 
 	void Start()
 	{
 		agent = GetComponent<NavMeshAgent>();
 
+		currentDestination = 0;
 	}
 
 	// Update is called once per frame
+
 	void Update () 
 	{
-		agent.SetDestination(target.position);
+		if(agent.remainingDistance <= 0)
+		{
+			print("Distance Remaining: " + agent.remainingDistance);
+
+			// Use to create a looping "patrol" behaviour
+			if(currentDestination >= target.Length)
+				currentDestination = 0;
+			else
+				currentDestination++;
+
+			// Use to create a random walkabout behaviour
+			//currentDestination = Random.Range(0,target.Length);
+
+			agent.SetDestination(target[currentDestination].position);
+		}
 	}
+
 }
