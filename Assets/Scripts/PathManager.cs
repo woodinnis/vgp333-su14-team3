06833494e77ccc2b60aea20ed73a3d3a638cab88
wood.innis <wood.innis@eab2e7ce-034a-a63b-0ccd-4006824de3b7>
2014-09-03 20:49:47 +0000

    Path randomization and patrol functionality implemented. EnemyController.cs allows multiple destinations.

---
+++ Assets/Scripts/PathManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathManager : MonoBehaviour {
+
+	[HideInInspector]
+	public static PathManager instance;
+
+	public GameObject[] nodeList;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		instance = this;			
+	}
+	
+	public Transform NewNode(Transform here)
+	{
+		foreach(GameObject TestHouse in nodeList)
+		{
+			if(TestHouse.transform != here.transform)
+			{
+				Transform newNode = Instantiate (TestHouse) as Transform;
+
+				return newNode;
+			}
+
+		}
+		Debug.Log("House: " + name + " not found.");
+		return null;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
