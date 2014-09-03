c81a9dfdd8e127fdc7152bd24e36cbb747f7e525
wood.innis <wood.innis@eab2e7ce-034a-a63b-0ccd-4006824de3b7>
2014-09-03 00:31:10 +0000

    Terrain Base, test houses, First-person Controller, and basic scripts included.

---
+++ trunk/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	public GameObject seekMe;
+
+	private NavMeshAgent agent;
+	private Vector3 target;
+
+	void Start()
+	{
+		agent = GetComponent<NavMeshAgent>();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		target = seekMe.transform.position;
+		agent.SetDestination(target);
+	}
+}
