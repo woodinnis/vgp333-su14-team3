13d4c49933809968ca51dbf1ef2b3fcfad536f26
wood.innis <wood.innis@eab2e7ce-034a-a63b-0ccd-4006824de3b7>
2014-09-09 21:12:59 +0000

    Added Car Collision Script

---
+++ trunk/Assets/Scripts/CarCollision.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarCollision : MonoBehaviour {
+
+	private Collision target;
+
+	void FixedUpdate()
+	{
+		target.rigidbody.AddForce (Vector3.back);
+	}
+
+	void OnCollisionEnter (Collision collision)
+	{
+		target = collision;
+	}
+
+}
+
