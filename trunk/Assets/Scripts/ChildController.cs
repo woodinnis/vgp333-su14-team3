52bdbf4b7fec649c520a805858796fca4461a6df
crazyscooter44@gmail.com <crazyscooter44@gmail.com@eab2e7ce-034a-a63b-0ccd-4006824de3b7>
2014-09-11 04:20:01 +0000

    [FinalProject]
    - Setup a playground prefab.
    - Added waypoints to Playground.
    - Added ChildController script.

---
+++ trunk/Assets/Scripts/ChildController.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChildController : MonoBehaviour 
+{
+	
+	private enum State
+	{
+		Idle,
+		Move,
+		Panic
+	}
+	
+	public float moveSpeed;
+	public float turnSpeed;
+	public float minIdleTime;
+	public float maxIdleTime;
+
+
+	
+	private State currentState;
+	private Transform myTransform;
+	private Vector3 destination;
+	private float idleTime;
+	
+	// Use this for initialization
+	void Start()
+	{
+		myTransform = transform;
+		currentState = State.Idle;
+		idleTime = Random.Range (minIdleTime, maxIdleTime);
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		UpdateFSM();
+	}
+	
+	private void UpdateFSM()
+	{
+		switch(currentState)
+		{
+		case State.Idle:
+			UpdateIdle();
+			break;
+		case State.Move:
+			UpdateMove();
+			break;
+		case State.Panic:
+			UpdatePanic();
+			break;
+		}
+	}
+	
+	private void UpdateIdle()
+	{
+		idleTime -= Time.deltaTime;
+		if(idleTime <= 0.0f)
+		{
+			currentState = State.Move;
+			destination = Playground.instance.navList[Random.Range (0,Playground.instance.navList.Length)].position;
+		}
+	}
+	
+	private void UpdateMove()
+	{
+		//To Do: Move Child to destination, when arrived change state to idle
+	}
+	private void UpdatePanic()
+	{
+		
+	}
+}
---
+++ trunk/Assets/Scripts/Playground.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Playground : MonoBehaviour 
+{
+	[HideInInspector]
+	public static Playground instance;
+	
+	public Transform[] navList;
+	
+	void Awake()
+	{
+		instance = this;
+	}
+}
